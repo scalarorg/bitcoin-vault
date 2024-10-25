606fe2535c32edd0d272811e426e845808d15b49
Vu Viet Tai <vuviettai@gmail.com>
2024-10-25 16:37:42 +0700

    Add mocha test for example package

--- example/src/params.ts
+++ example/src/params.ts
@@ -1,5 +1,5 @@
 import * as bitcoin from "bitcoinjs-lib";
-import { extractPublicKeyFromWIF } from "bitcoin-vault";
+import { extractPublicKeyFromWIF } from "@scalar/bitcoin-vault";
 export interface ConfigParams {
     networkName: string;
     network: bitcoin.Network;
