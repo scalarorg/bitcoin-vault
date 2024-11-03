ac2703114794f6ff3da0312be94654bd43382402
David Tran <tranhuyducseven@gmail.com>
2024-11-03 10:24:15 +0700

    fix: load script

--- binding/build.ts
+++ binding/build.ts
@@ -10,25 +10,28 @@ console.log("Building...");
 // Build for browser
 await Bun.build({
   entrypoints: ["./src/index.ts"],
-  // external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
   external: Object.keys(dependencies),
   format: "esm",
   minify: true,
   outdir: "./dist",
   sourcemap: "external",
   target: "browser",
+  naming: {
+    entry: "[dir]/[name].[ext]",
+    chunk: "[dir]/[name].[ext]",
+  },
 });
 
 // Build for node
-// await Bun.build({
-//   entrypoints: ["./src/index.ts"],
-//   external: Object.keys(dependencies),
-//   format: "esm",
-//   minify: true,
-//   outdir: "./node",
-//   sourcemap: "external",
-//   target: "node",
-// });
+await Bun.build({
+  entrypoints: ["./src/index.ts"],
+  external: Object.keys(dependencies),
+  format: "esm",
+  minify: true,
+  outdir: "./dist/node",
+  sourcemap: "external",
+  target: "node",
+});
 
 console.log("JSCompiling", "Done!");
 console.log("TypeCompiling", "Building...");
@@ -43,13 +46,12 @@ fs.mkdirSync("./dist", { recursive: true });
 // Write typed content to index.d.ts for both browser and node
 fs.writeFileSync("./dist/index.d.ts", typedContentForBrowser.join("\n"));
 
-// const typedContent = generateDtsBundle([
-//   {
-//     filePath: "./src/index.ts",
-//   },
-// ]);
+const typedContent = generateDtsBundle([
+  {
+    filePath: "./src/index.ts",
+  },
+]);
 
-// fs.mkdirSync("./node", { recursive: true });
-// fs.writeFileSync("./node/index.d.ts", typedContent.join("\n"));
-// console.log("TypeCompiling", "Done!");
-// console.log("Build", `Build success, take ${Date.now() - start}ms`);
+fs.mkdirSync("./dist/node", { recursive: true });
+fs.writeFileSync("./dist/node/index.d.ts", typedContent.join("\n"));
+console.log("TypeCompiling", "Done!");
