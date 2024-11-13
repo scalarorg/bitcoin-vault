3e0258bf062917fd6219f959985161a9e6e12979
David Tran <tranhuyducseven@gmail.com>
2024-11-13 20:51:32 +0700

    feat: add script to test descriptor importing

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,149 +1,202 @@
 #!/bin/sh
-# wget https://bitcoincore.org/bin/bitcoin-core-26.0/bitcoin-26.0-x86_64-linux-gnu.tar.gz -o /root/bitcoin-26.0-x86_64-linux-gnu.tar.gz
-# wget https://bitcoincore.org/bin/bitcoin-core-26.0/bitcoin-26.0-x86_64-linux-gnu.tar.gz
-# tar -xvf bitcoin-26.0-x86_64-linux-gnu.tar.gz
-# sudo ln -sf bitcoin-26.0/bin/bitcoind /usr/bin/bitcoind
-# bitcoind
-COVENANT_COUNT=5
-start_bitcoind() {
-    bitcoind -testnet \
-      -rpcbind=${RPC_BIND:-127.0.0.1:18332} \
-      -rpcuser=${RPC_USER:-user} \
-      -rpcpassword=${RPC_PASS:-password} \
-      -rpcallowip=${RPC_ALLOWIP:-127.0.0.1/0} \
-      -datadir=${DATADIR:-/data/.bitcoin} \
-      -server=${SERVER:-1} \
-      -txindex=${TXINDEX:-1} \
-      -connect=${CONNECT:-0} \
-      -daemon=${DAEMON:-1}
-}
-createwallet_legacy() {
-    WALLET_LEGACY_NAME=${1:-legacy}
-    WALLET_LEACY_PASSPHRASE=${2:-passphrase}
-    bitcoin-cli -named createwallet \
-        wallet_name=${WALLET_LEGACY_NAME:-name} \
-        passphrase=${WALLET_LEACY_PASSPHRASE:-passphrase} \
-        load_on_startup=true \
-        descriptors=false # create legacy wallet
+run() {
+    NAME=${1:-"bitcoin-regtest"}
+    docker run --rm -d \
+        --name ${NAME} \
+        -p 18332:18332 \
+        -p 18333:18333 \
+        -v $(pwd)/.bitcoin:/root/.bitcoin \
+        -v $(pwd)/bitcoin.conf:/root/.bitcoin/bitcoin.conf \
+        -v $(pwd)/bitcoin.sh:/root/bitcoin.sh \
+        -e DATADIR=/root/.bitcoin \
+        -u root \
+        -w /root/.bitcoin \
+        --entrypoint /bin/sh \
+        lncm/bitcoind:v25.0 /root/bitcoin.sh entrypoint
 }
 
 createwallet_descriptors() {
-    WALLET_DESCRIPTORS_NAME=${1:-descriptors}
-    WALLET_DESCRIPTORS_PASSPHRASE=${2:-passphrase}
+    WALLET_NAME=${1:-staker}
+    WALLET_PASSPHRASE=${2:-passphrase}
     bitcoin-cli -named createwallet \
-        wallet_name=${WALLET_DESCRIPTORS_NAME:-name} \
-        passphrase=${WALLET_DESCRIPTORS_PASSPHRASE:-passphrase} \
-        load_on_startup=false \
-        descriptors=true # create legacy wallet
-}
-
-getnewlegacyaddress() {
-    WORKDIR=${DATADIR:-/data/.bitcoin}
-    ADDRESS_TYPE=${1:-legacy}
-    NAME=${2:-${ADDRESS_TYPE}}
-    LABEL="address-${ADDRESS_TYPE}-${NAME}"
-    BTC_ADDRESS=$(bitcoin-cli -rpcwallet=${WALLET_LEGACY_NAME} getnewaddress $LABEL $ADDRESS_TYPE)
-    echo $BTC_ADDRESS>$WORKDIR/${LABEL}.txt
-    bitcoin-cli -rpcwallet=${WALLET_LEGACY_NAME} walletpassphrase ${WALLET_LEACY_PASSPHRASE:-passphrase} 60
-    bitcoin-cli -rpcwallet=${WALLET_LEGACY_NAME} getaddressinfo $BTC_ADDRESS>$WORKDIR/${LABEL}-info.txt
-    bitcoin-cli -rpcwallet=${WALLET_LEGACY_NAME} dumpprivkey $BTC_ADDRESS>$WORKDIR/${LABEL}-privkey.txt
-    bitcoin-cli -rpcwallet=${WALLET_LEGACY_NAME} generatetoaddress 101 ${BTC_ADDRESS}
+        wallet_name=${WALLET_NAME} \
+        passphrase=${WALLET_PASSPHRASE} \
+        load_on_startup=true \
+        descriptors=true # Use descriptors for Taproot and P2WPKH addresses
 }
 
 getnewtaprootaddress() {
     WORKDIR=${DATADIR:-/data/.bitcoin}
-    ADDRESS_TYPE=bech32m
-    NAME=${1:-${ADDRESS_TYPE}}
-    LABEL="address-${ADDRESS_TYPE}-${NAME}"
-    BTC_ADDRESS=$(bitcoin-cli -rpcwallet=${WALLET_DESCRIPTORS_NAME} getnewaddress $LABEL $ADDRESS_TYPE)
-    echo $BTC_ADDRESS>$WORKDIR/${LABEL}.txt
-    bitcoin-cli -rpcwallet=${WALLET_DESCRIPTORS_NAME} walletpassphrase ${WALLET_DESCRIPTORS_PASSPHRASE:-passphrase} 60
-    bitcoin-cli -rpcwallet=${WALLET_DESCRIPTORS_NAME} getaddressinfo $BTC_ADDRESS>$WORKDIR/${LABEL}-info.txt
-    # Only legacy addresses can be dumped
-    # bitcoin-cli -rpcwallet=${WALLET_DESCRIPTORS_NAME} dumpprivkey $BTC_ADDRESS>$WORKDIR/${LABEL}-privkey.txt
-    bitcoin-cli -rpcwallet=${WALLET_DESCRIPTORS_NAME} generatetoaddress 101 ${BTC_ADDRESS}
+    LABEL="p2tr"
+    BTC_ADDRESS=$(bitcoin-cli -rpcwallet=${WALLET_NAME} getnewaddress $LABEL bech32m)
+    echo $BTC_ADDRESS >$WORKDIR/${LABEL}.txt
+    bitcoin-cli -rpcwallet=${WALLET_NAME} walletpassphrase ${WALLET_PASSPHRASE:-passphrase} 60
+    bitcoin-cli -rpcwallet=${WALLET_NAME} getaddressinfo $BTC_ADDRESS >$WORKDIR/${LABEL}-info.txt
+    bitcoin-cli -rpcwallet=${WALLET_NAME} generatetoaddress 101 ${BTC_ADDRESS}
 }
 
-create_envs() {
+getnewp2wpkhaddress() {
     WORKDIR=${DATADIR:-/data/.bitcoin}
-    ENV_FILE=${WORKDIR}/.env.btc
-    STAKER_ADDRESS=$(cat ${WORKDIR}/address-bech32-staker.txt)
-    STAKER_PRIVKEY=$(cat ${WORKDIR}/address-bech32-staker-privkey.txt)
-    STAKER_PUBKEY=$(cat ${WORKDIR}/address-bech32-staker-info.txt | jq -r '.pubkey')
-    SERVICE_ADDRESS=$(cat ${WORKDIR}/address-bech32-service.txt)
-    SERVICE_PRIVKEY=$(cat ${WORKDIR}/address-bech32-service-privkey.txt)
-    SERVICE_PUBKEY=$(cat ${WORKDIR}/address-bech32-service-info.txt | jq -r '.pubkey')
-    PROTOCOL_ADDRESS=$(cat ${WORKDIR}/address-bech32-protocol.txt)
-    PROTOCOL_PRIVKEY=$(cat ${WORKDIR}/address-bech32-protocol-privkey.txt)
-    PROTOCOL_PUBKEY=$(cat ${WORKDIR}/address-bech32-protocol-info.txt | jq -r '.pubkey')
-    echo "BOND_HOLDER_ADDRESS=${STAKER_ADDRESS}" > ${ENV_FILE}
-    echo "BOND_HOLDER_PRIVATE_KEY=${STAKER_PRIVKEY}" >> ${ENV_FILE}
-    echo "BOND_HOLDER_PUBLIC_KEY=${STAKER_PUBKEY}" >> ${ENV_FILE}
-    echo "PROTOCOL_ADDRESS=${PROTOCOL_ADDRESS}" >> ${ENV_FILE}
-    echo "PROTOCOL_PUBLIC_KEY=${PROTOCOL_PUBKEY}" >> ${ENV_FILE}
-    echo "PROTOCOL_PRIVATE_KEY=${PROTOCOL_PRIVKEY}" >> ${ENV_FILE}
-    echo "SERVICE_ADDRESS=${SERVICE_ADDRESS}" >> ${ENV_FILE}
-    echo "SERVICE_PUBKEY=${SERVICE_PUBKEY}" >> ${ENV_FILE}
-
-    COVENANT_PUBKEYS=$(cat ${WORKDIR}/address-bech32-covenant1-info.txt | jq -r '.pubkey')
-    COVENANT_PRIVKEYS=$(cat ${WORKDIR}/address-bech32-covenant1-privkey.txt)
-    for i in $(seq 2 ${COVENANT_COUNT:-5})
-	do
-        PUBKEY=$(cat ${WORKDIR}/address-bech32-covenant${i}-info.txt | jq -r '.pubkey')
-        PRIVKEY=$(cat ${WORKDIR}/address-bech32-covenant${i}-privkey.txt)
-        COVENANT_PUBKEYS="${COVENANT_PUBKEYS},${PUBKEY}"
-        COVENANT_PRIVKEYS="${COVENANT_PRIVKEYS},${PRIVKEY}"
-	done
-    echo "COVENANT_PUBKEYS=${COVENANT_PUBKEYS}" >> ${ENV_FILE}
-    echo "COVENANT_PRIVKEYS=${COVENANT_PRIVKEYS}" >> ${ENV_FILE}
+    LABEL="p2wpkh"
+    BTC_ADDRESS=$(bitcoin-cli -rpcwallet=${WALLET_NAME} getnewaddress $LABEL bech32)
+    echo $BTC_ADDRESS >$WORKDIR/${LABEL}.txt
+    bitcoin-cli -rpcwallet=${WALLET_NAME} walletpassphrase ${WALLET_PASSPHRASE:-passphrase} 60
+    bitcoin-cli -rpcwallet=${WALLET_NAME} getaddressinfo $BTC_ADDRESS >$WORKDIR/${LABEL}-info.txt
+    bitcoin-cli -rpcwallet=${WALLET_NAME} generatetoaddress 101 ${BTC_ADDRESS}
 }
- # create block for each 30s
+
 generate_blocks() {
     WORKDIR=${DATADIR:-/data/.bitcoin}
-    while :
-    do
-        bitcoin-cli -rpcwallet=${WALLET_LEGACY_NAME} -generate 1
+    while :; do
+        echo "Generating blocks to p2tr"
+        bitcoin-cli -rpcwallet=${WALLET_NAME} generatetoaddress 1 $(cat $WORKDIR/p2tr.txt)
+        echo "Generating blocks to p2wpkh"
+        bitcoin-cli -rpcwallet=${WALLET_NAME} generatetoaddress 1 $(cat $WORKDIR/p2wpkh.txt)
         sleep 30
     done
 }
+
 entrypoint() {
     apk add --no-cache jq
     WORKDIR=${DATADIR:-/data/.bitcoin}
-    rm -rf ${WORKDIR}/address-*
+    rm -rf ${WORKDIR}/p2tr*
+    rm -rf ${WORKDIR}/p2wpkh*
     bitcoind
     while ! nc -z 127.0.0.1 18332; do
         sleep 1
     done
     sleep 15
-    createwallet_descriptors
-    # bitcoin-cli loadwallet ${WALLET_DESCRIPTORS_NAME}
-    # Taproot
-    getnewtaprootaddress staker
-    getnewtaprootaddress service
-    getnewtaprootaddress protocol
 
-    for i in $(seq ${COVENANT_COUNT:-5})
-    do
-        getnewtaprootaddress bech32 covenant${i}
-    done
+    # Create wallet and addresses
+    # createwallet_descriptors staker
+    # getnewtaprootaddress
+    # getnewp2wpkhaddress
 
-    createwallet_legacy
-    # bitcoin-cli loadwallet ${WALLET_LEGACY_NAME}
-    # getnewlegacyaddress legacy
-    # getnewlegacyaddress p2sh-segwit
-    # P2WSH
-    getnewlegacyaddress bech32 staker
-    getnewlegacyaddress bech32 service
-    getnewlegacyaddress bech32 protocol
-
-    for i in $(seq ${COVENANT_COUNT:-5})
-    do
-        getnewlegacyaddress bech32 covenant${i}
-    done
-    create_envs
-    # create_envs_from_template
-    generate_blocks
+    # generate_blocks
     sleep infinity
 }
 
-$@   
+create_psbt() {
+    FROM_ADDRESS=$(jq -r '.address' "$WORKDIR/root/.bitcoin/p2tr-info.txt")
+    TO_ADDRESS=$(jq -r '.address' "$WORKDIR/root/.bitcoin/p2wpkh-info.txt")
+
+    # Get the UTXO details
+    UTXOS=$(bitcoin-cli -rpcwallet=staker listunspent 6 9999999 "[\"${FROM_ADDRESS}\"]")
+    TXID=$(echo $UTXOS | jq -r '.[0].txid')
+    VOUT=$(echo $UTXOS | jq -r '.[0].vout')
+
+    echo "TXID: $TXID"
+    echo "VOUT: $VOUT"
+    echo "UTXOS[0]: $(echo $UTXOS | jq -r '.[0]')"
+    echo "FROM_ADDRESS: $FROM_ADDRESS"
+    echo "TO_ADDRESS: $TO_ADDRESS"
+
+    # Create PSBT
+    PSBT_RESULT=$(bitcoin-cli -rpcwallet=staker walletcreatefundedpsbt \
+        "[{\"txid\":\"${TXID}\",\"vout\":${VOUT}}]" \
+        "[{\"${TO_ADDRESS}\": 30}]" \
+        0 '{"replaceable": true, "feeRate": 0.0001}')
+
+    echo "PSBT Result: $PSBT_RESULT"
+
+    bitcoin-cli -rpcwallet=staker walletpassphrase ${WALLET_PASSPHRASE:-passphrase} 60
+
+    # Process PSBT using wallet
+    SIGNED_RESULT=$(bitcoin-cli -rpcwallet=staker walletprocesspsbt $(echo $PSBT_RESULT | jq -r '.psbt'))
+
+    echo "Signed Result: $SIGNED_RESULT"
+
+    if [ "$(echo $SIGNED_RESULT | jq -r '.complete')" = "true" ]; then
+        # If signing is complete, finalize and broadcast
+        FINAL_RESULT=$(bitcoin-cli finalizepsbt $(echo $SIGNED_RESULT | jq -r '.psbt'))
+        if [ "$(echo $FINAL_RESULT | jq -r '.complete')" = "true" ]; then
+            # Broadcast the transaction
+            TXID=$(bitcoin-cli sendrawtransaction $(echo $FINAL_RESULT | jq -r '.hex'))
+            echo "Transaction broadcast: $TXID"
+        else
+            echo "Failed to finalize PSBT"
+        fi
+    else
+        echo "Failed to sign PSBT completely"
+    fi
+}
+
+import_private_key() {
+    WALLET_NAME=${1:-staker}
+    PRIVATE_KEY=${2:-"cQT95AF79E2WrWbxKEsZq3uH2GZj2gs34b7NGW8CPnL16po68CBg"}
+    LABEL=${3:-"imported_key"}
+
+    WALLET_PASSPHRASE=${WALLET_PASSPHRASE:-passphrase}
+    bitcoin-cli -rpcwallet=${WALLET_NAME} walletpassphrase ${WALLET_PASSPHRASE} 60
+
+    # Create the ranged descriptor with the private key
+    TR_DESC=$(bitcoin-cli getdescriptorinfo "tr($PRIVATE_KEY)")
+    echo "TR_DESC: $TR_DESC"
+
+    # Import the descriptor with a range
+    bitcoin-cli -rpcwallet=${WALLET_NAME} importdescriptors '[
+        {
+            "desc": "'$(echo $TR_DESC | jq -r '.descriptor')'",
+            "timestamp": "now",
+            "label": "'$LABEL'_tr",
+            "internal": false,
+            "active": true,
+            "range": [0,0]
+        }
+    ]'
+}
+
+p2tr() {
+    WALLET_NAME=staker
+    PRIVATE_KEY="cQ7kMt56n8GeKkshaiCt3Lh2ChuaD3tWdSrH37MwU93PA4qZs9JR"
+    WALLET_PASSPHRASE=passphrase
+    # XONLY_PUBKEY="155f0dd5185e65acd2167b9528a92b9fccaa6cf914a3c10416abd3e5de77377d"
+
+    # Unlock the wallet
+    bitcoin-cli -rpcwallet=${WALLET_NAME} walletpassphrase ${WALLET_PASSPHRASE} 60
+
+    ORIGINAL_DESC="tr(${PRIVATE_KEY})"
+
+    DESC_INFO=$(bitcoin-cli getdescriptorinfo "$ORIGINAL_DESC")
+
+    echo "DESC_INFO: $DESC_INFO"
+
+    CHECKSUM=$(echo "$DESC_INFO" | jq -r '.checksum')
+
+    echo "CHECKSUM: $CHECKSUM"
+
+    RESULT=$(bitcoin-cli importdescriptors '[{ "desc": "'"$ORIGINAL_DESC"'#'"$CHECKSUM"'", "timestamp": "now", "internal": true }]')
+
+    echo "RESULT: $RESULT"
+
+    ADDRESS_ARRAY=$(bitcoin-cli deriveaddresses "$ORIGINAL_DESC#$CHECKSUM")
+
+    echo "Derived address: $ADDRESS_ARRAY"
+
+    ADDRESS=$(echo $ADDRESS_ARRAY | jq -r '.[0]')
+
+    UTXOS=$(bitcoin-cli -rpcwallet=staker listunspent 6 9999999 "[\"${ADDRESS}\"]")
+
+    echo "Before: $UTXOS"
+
+    ## fund the address
+    bitcoin-cli -rpcwallet=${WALLET_NAME} generatetoaddress 1 $ADDRESS
+
+    sleep 10
+
+    UTXOS=$(bitcoin-cli -rpcwallet=staker listunspent 6 9999999 "[\"${ADDRESS}\"]")
+
+    echo "After: $UTXOS"
+}
+
+list() {
+
+    FROM_ADDRESS="tb1p5hpkty3ykt92qx6m0rastprnreqx6dqexagg8mgp3hgz53p9lk3qd2c4f2"
+
+    UTXOS=$(bitcoin-cli -rpcwallet=staker listunspent 6 9999999 "[\"${FROM_ADDRESS}\"]")
+
+    echo "UTXOS: $UTXOS"
+
+}
+
+$@
